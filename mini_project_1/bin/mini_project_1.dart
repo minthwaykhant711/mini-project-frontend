1f2c98c7b4b8bc1e446e6b6d4db9f7dddd4038c0
6631501187-spec <6631501187@lamduan.mfu.ac.th>
2025-08-26 00:13:58 +0700

    addedExpenses

--- mini_project_1/bin/mini_project_1.dart
+++ mini_project_1/bin/mini_project_1.dart
@@ -1,5 +1,39 @@
 import 'package:mini_project_1/mini_project_1.dart' as mini_project_1;
+import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'dart:convert';
 
 void main(List<String> arguments) {
   print('Hello world: ${mini_project_1.calculate()}!');
+}
+
+Future<void> addNewExpense(String userId) async {
+ print("\n======== Add new item ========");
+ stdout.write("Item: ");
+ String? item = stdin.readLineSync()?.trim();
+ stdout.write("Paid: ");
+ String? paidStr = stdin.readLineSync()?.trim();
+  if (item == null || paidStr == null) {
+   print("Incomplete input");
+   return;
+ }
+  try {
+   int paid = int.parse(paidStr);
+   final url = Uri.parse('http://localhost:3000/expenses');
+   final body = {
+     "userId": userId,
+     "item": item,
+     "paid": paid,
+   };
+   final response = await http.post(url, headers: {"Content-Type": "application/json"}, body: jsonEncode(body));
+
+
+   if (response.statusCode == 201) {
+     print("Inserted!");
+   } else {
+     print('Connection error! Status code: ${response.statusCode}');
+   }
+ } catch (e) {
+   print("Invalid paid amount. Please enter a number.");
+ }
 }
